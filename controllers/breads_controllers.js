fe89015dfd297bee63f211063aa5c2a9e3616bf5
AliPort <bwithtacos@gmail.com>
2022-08-01 20:51:28 -0500

    adding helper methods

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -1,18 +1,30 @@
 const express =require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const mongoose = require('mongoose')
 
 
 //index
-breads.get('/', (req,res) => {
-    res.render ('index',
-    {
-        breads: Bread,
-        title: 'Index Page'
-    }
-    )
-    //res.send(Bread)
-})
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
+      })
+
+
+  // )
+          console.log(foundBreads)
+      })
+  // res.render('index',
+  //   {
+  //     breads: Bread,
+  //     title: 'Index Page'
+  //   }
+  // )
+//
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -21,9 +33,27 @@ breads.get('/:arrayIndex', (req, res) => {
         bread:Bread[req.params.arrayIndex]
       })
     } else {
-      res.send('404')
+      res.render('404')
     }
   })
-  
-  
+//NEW
+
+//Edit
+breads.post('/', (req, res) => {
+  if(!req.body.image) {
+      req.body.image = undefined 
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.create(req.body)
+  res.redirect('/breads')
+})
+
+
+   
 module.exports = breads
+
+module.exports = Bread
--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-//dependencies
+// DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
+// CONFIGURATION
 require('dotenv').config()
-
-//configuration 
-const PORT = process.env.PORT   
-    console.log(PORT)
-
+const PORT = process.env.PORT
 const app = express()
 
-//middleware
-app.set('views', __dirname+'/views')
+//Mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
+
+// MIDDLEWARE
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+//app.set('views,_dirname+'/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
 
-//routes
-app.get('/', (req,res) => {
-    res.send('Welcome to an Awesome App about Breads!')
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an awesome App about Breads')
 })
 
-
-  
-
-//listen
-
-
-//breads
-const breadsController = require('./controllers/breads_controllers.js')
+// Breads
+const breadsController = require('./controllers/breads_controllers')
 app.use('/breads', breadsController)
 
 // 404 Page
 app.get('*', (req, res) => {
-    res.send('404')
-  })
+  res.send('404')
+})
 
-app.listen(PORT, ()=>{
-
+//listen
+app.listen(PORT,() =>{
+  console.log('listening on port',PORT);
 })
